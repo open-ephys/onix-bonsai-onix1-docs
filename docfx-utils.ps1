58cab30dbaefe61eda14e3b2d4acfe8a9d14ac3d
cjsha <cs@open-ephys.org>
2025-01-21 17:29:43 -0500

    fix lychee cmd to avoid 429 errors

--- docfx-utils.ps1
+++ docfx-utils.ps1
@@ -22,7 +22,7 @@ function lychee($lycheePath)
 {
     Write-Output "`nRunning lychee..."
     Write-Output "------------------------------------------`n"
-    Invoke-Expression "& `"$lycheePath`" --no-progress --base _site --exclude ^https://github\.com.*merge.* --exclude ^https://github\.com.*apiSpec.* --exclude ^https://github\.com/open-ephys/onix1-bonsai-docs/blob/.*/#L1 '_site/**/*.html'"
+    Invoke-Expression "& `"$lycheePath`" --no-progress --base _site --exclude ^https://github\.com.*merge.* --exclude ^https://github\.com.*apiSpec.* --exclude ^https://github\.com/open-ephys/onix1-bonsai-docs/blob/.*/#L1 '_site/**/*.html' --max-retries 0 --max-concurrency 32 --cache --max-cache-age 1d"
     Write-Output "`n"
 }
 
