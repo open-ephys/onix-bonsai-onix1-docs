6f8a1d334f1ddd96be2ee5571caab011db3944ee
cjsha <cs@open-ephys.org>
2025-02-14 22:56:24 -0500

    Simplify loading scripts
    - Reduce dependencies
    - link out to Google Colab

--- workflows/hardware/np1e/load-np1e.py
+++ workflows/hardware/np1e/load-np1e.py
@@ -1,129 +1,132 @@
-import os
+# Import necessary packages
 import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib.patches as mpatches
 import spikeinterface.extractors as se
-import spikeinterface.widgets as sw
 import probeinterface
 import probeinterface.plotting
 
-ap_gain = 500 # Change to the ap band gain used
-lfp_gain = 500 # Change to the lfp band gain used
-suffix = 0 # Change to match file names' suffix
-num_channels = 384 # Decrease channels to expedite plotting and inspect fewer traces
-# Change this to the directory of your data. In this example, data's in the same directory as this data loading Python script
-data_directory = os.path.dirname(os.path.realpath(__file__))
-mode = 'auto' # This uses colormap plot above 50 channels and line plot below 50 channels. Refer to the spikeinterface docs for more options
-# Change this to the name of your probeinterface file (should be in the same directory as the rest of your data for this example script to work as-is)
-probeinterface_filename = 'np1-config.json'
+# Parameters
+suffix = 0              # Change to match file names' suffix
+start_t = 2.0           # When to start plotting ephys data (second)
+dur = 5.0               # Duration of ephys data to plot (seconds)
+plot_num_channels = 10  # Number of channels to plot
+ap_gain = 1000          # Change to the ap band gain used
+lfp_gain = 50           # Change to the lfp band gain used
 
+# Constants
+fs_hz_ap = 30e3
+fs_hz_lfp = fs_hz_ap / 12
+sample_offset = 512
+sample_gain = 1171.875
+num_channels = 384
 
-plt.close('all')
-
-#%% Metadata
+# Load acquisition session data
 dt = {'names': ('time', 'acq_clk_hz', 'block_read_sz', 'block_write_sz'),
       'formats': ('datetime64[us]', 'u4', 'u4', 'u4')}
-meta = np.genfromtxt(os.path.join(data_directory, f'start-time_{suffix}.csv'), delimiter=',', dtype=dt, skip_header=1)
+meta = np.genfromtxt(f'/content/drive/MyDrive/np1e/start-time_{suffix}.csv', delimiter=',', dtype=dt, skip_header=1)
 print(f"Recording was started at {meta['time']} GMT")
 
-#%% Neuropixels 1.0 probeinterface
-fig, ax = plt.subplots()
-np1_config = probeinterface.io.read_probeinterface(os.path.join(data_directory,  'np1-config.json'))
-contacts_colors = ['cyan' if device_channel_index > -1 else 'red' for device_channel_index in np1_config.probes[0].device_channel_indices]
-probeinterface.plotting.plot_probegroup(np1_config, ax=ax, contacts_colors=contacts_colors, contacts_kargs={'alpha' : 1, 'zorder' : 10}, show_channel_on_click=True)
-fig.set_size_inches(2, 9)
-enabled = mpatches.Patch(color='cyan', label='Enabled')
-disabled = mpatches.Patch(color='red', label='Disabled')
-fig.legend(handles=[enabled, disabled], loc='outside upper center') 
-plt.tight_layout()
+# Load Neuropixels 1.0 AP clock data
+ap_time = np.fromfile(f'/content/drive/MyDrive/np1e/np1-clock_{suffix}.raw', dtype=np.uint64).astype(np.double) / meta['acq_clk_hz']
 
+# Load and scale Neuropixels 1.0 AP data
+gain_to_uV_ap = sample_gain / ap_gain
+offset_to_uV_ap = -sample_offset * gain_to_uV_ap
+ap_rec = se.read_binary(f'/content/drive/MyDrive/np1e/np1-spike_{suffix}.raw',
+                        sampling_frequency=fs_hz_ap,
+                        dtype=np.uint16,
+                        num_channels=num_channels,
+                        gain_to_uV=gain_to_uV_ap,
+                        offset_to_uV=offset_to_uV_ap)
+ap_scaled = ap_rec.get_traces(return_scaled=True, channel_ids=np.arange(plot_num_channels))
 
-#%% Neuropixels 1.0 Data
-bit_depth = 10
-fig, ax = plt.subplots(1,2)
-fig.suptitle('Neuropixels 1.0 Data')
-fig.set_size_inches(9, 9)
-plt.subplots_adjust(wspace=0.3)
+# Truncate data based on start_t & dur
+b = np.bitwise_and(ap_time >= start_t, ap_time < start_t + dur)
+ap_scaled_truncated = ap_scaled[b]
+ap_time_truncated = ap_time[b]
 
-ap_scalar = 1.2e6 / (2 ** bit_depth) / ap_gain
-ap_offset = (2 ** (bit_depth - 1)) * ap_scalar
-ap_recording = se.read_binary(os.path.join(data_directory,  f"np1-spike_{suffix}.raw"), 
-                           3e5, 
-                           np.uint16, 
-                           num_channels, 
-                           gain_to_uV=ap_scalar, 
-                           offset_to_uV=-ap_offset) 
-ap_traces_plot = sw.plot_traces(ap_recording, 
-                   backend='matplotlib', 
-                   return_scaled=True, 
-                   mode=mode, 
-                   clim=(-ap_offset,ap_offset),
-                   ax=ax[0])
-ax[0].set_xlabel("time (sec)")
-ax[0].set_ylabel("AP (µV)")
+# Plot scaled Neuropixels 1.0 AP data
+fig = plt.figure()
+fig.suptitle('Neuropixels 1.0 AP Data')
+plt.plot(ap_time_truncated, ap_scaled_truncated)
+plt.xlabel("Time (seconds)")
+plt.ylabel("Voltage (µV)")
 
-lfp_scalar = 1.2e6 / (2 ** bit_depth) / lfp_gain
-lfp_offset = (2 ** (bit_depth - 1)) * lfp_scalar
-lfp_recording = se.read_binary(os.path.join(data_directory,  f"np1-lfp_{suffix}.raw"), 
-                           3e5/12, 
-                           np.uint16, 
-                           num_channels, 
-                           gain_to_uV=lfp_scalar, 
-                           offset_to_uV=-lfp_offset) 
-lfp_traces_plot = sw.plot_traces(lfp_recording, 
-                   backend='matplotlib', 
-                   return_scaled=True, 
-                   mode=mode, 
-                   clim=(-lfp_offset,lfp_offset), 
-                  ax=ax[1])
-ax[1].set_xlabel("time (sec)")
-ax[1].set_ylabel("LFP (µV)")
+# Load Neuropixels 1.0 LFP clock data
+lfp_time = ap_time[::12]
 
-#%% Bno055
+# Load and scale Neuropixels 1.0 LFP data
+gain_to_uV_lfp = sample_gain / lfp_gain
+offset_to_uV_lfp = -sample_offset * gain_to_uV_lfp
+lfp_rec = se.read_binary(f'/content/drive/MyDrive/np1e/np1-lfp_{suffix}.raw',
+                         sampling_frequency=fs_hz_lfp,
+                         dtype=np.uint16,
+                         num_channels=num_channels,
+                         gain_to_uV=gain_to_uV_lfp,
+                         offset_to_uV=offset_to_uV_lfp)
+lfp_scaled = lfp_rec.get_traces(return_scaled=True, channel_ids=np.arange(plot_num_channels))
+
+# Truncate data based on start_t & dur
+b = np.bitwise_and(lfp_time >= start_t, lfp_time < start_t + dur)
+lfp_scaled_truncated = lfp_scaled[b]
+lfp_time_truncated = lfp_time[b]
+
+# Plot scaled Neuropixels 1.0 LFP data
+fig = plt.figure()
+fig.suptitle('Neuropixels 1.0 LFP Data')
+plt.plot(lfp_time_truncated, lfp_scaled_truncated)
+plt.xlabel("Time (seconds)")
+plt.ylabel("Voltage (µV)")
+
+# Load BNO055 data
 dt = {'names': ('clock', 'euler', 'quat', 'is_quat_id', 'accel', 'grav', 'temp'),
-      'formats': ('u8', '(1,3)f8', '(1,4)f8', '?', '(1,3)f8', '(1,3)f8', 'f8', '?')}
-bno055 = np.genfromtxt(os.path.join(data_directory, f'bno055_{suffix}.csv'), delimiter=',', dtype=dt)
+      'formats': ('u8', '(1,3)f8', '(1,4)f8', '?', '(1,3)f8', '(1,3)f8', 'f8')}
+bno055 = np.genfromtxt(f'/content/drive/MyDrive/np1e/bno055_{suffix}.csv', delimiter=',', dtype=dt)
 
+# Load BNO055 clock data
 bno055_time = bno055['clock'] / meta['acq_clk_hz']
 
-plt.figure()
+# Plot BNO055 data
+fig = plt.figure()
+fig.suptitle('BNO055 Data')
 plt.subplot(231)
+
 plt.plot(bno055_time, bno055['euler'].squeeze())
-plt.xlabel("time (sec)")
-plt.ylabel("angle (deg.)")
-plt.ylim(-185, 365)
+plt.xlabel("Time (seconds)")
+plt.ylabel("Angle (degrees)")
 plt.legend(['yaw', 'pitch', 'roll'])
 plt.title('Euler')
 
 plt.subplot(232)
 plt.plot(bno055_time, bno055['quat'].squeeze())
-plt.xlabel("time (sec)")
-plt.ylim(-1.1, 1.1)
+plt.xlabel("Time (seconds)")
 plt.legend(['X', 'Y', 'Z', 'W'])
 plt.title('Quaternion')
 
 plt.subplot(233)
 plt.plot(bno055_time, bno055['accel'].squeeze())
-plt.xlabel("time (sec)")
-plt.ylabel("accel. (m/s^2)")
+plt.xlabel("Time (seconds)")
+plt.ylabel("Acceleration (m/s\u00b2)")
 plt.legend(['X', 'Y', 'Z'])
-plt.title('Lin. Accel.')
+plt.title('Linear Acceleration')
 
 plt.subplot(234)
 plt.plot(bno055_time, bno055['grav'].squeeze())
-plt.xlabel("time (sec)")
-plt.ylabel("accel. (m/s^2)")
+plt.xlabel("Time (seconds)")
+plt.ylabel("Acceleration (m/s\u00b2)")
 plt.legend(['X', 'Y', 'Z'])
-plt.title('Gravity Vec.')
+plt.title('Gravity Vector')
 
 plt.subplot(235)
 plt.plot(bno055_time, bno055['temp'].squeeze())
-plt.xlabel("time (sec)")
-plt.ylabel("temp. (°C)")
-plt.title('Headstage Temp.')
+plt.xlabel("Time (seconds)")
+plt.ylabel("Temperature (°C)")
+plt.title('Headstage Temperature')
 
 plt.tight_layout()
 
-plt.show()
+# Load and plot Neuropixels 1.0 probeinterface probe group
+np1_config = probeinterface.io.read_probeinterface('/content/drive/MyDrive/np1e/np1-config.json')
+probeinterface.plotting.plot_probegroup(np1_config, show_channel_on_click=True)
 
-fig.suptitle('BNO055 Data')
+plt.show()
--- workflows/hardware/rhs2116/load-rhs2116.py
+++ workflows/hardware/rhs2116/load-rhs2116.py
@@ -1,62 +1,64 @@
-import os
+# Import necessary packages
 import numpy as np
 import matplotlib.pyplot as plt
 
-plt.close('all')
+# Parameters
+suffix = 0              # Change to match file names' suffix
+start_t = 2.0           # When to start plotting ephys data (second)
+dur = 5.0               # Duration of ephys data to plot (seconds)
+plot_num_channels = 10  # Number of channels to plot
 
-#%% Set Parameters for Loading Data
+# Constants
+fs_hz = 30e3
+ac_uV_multiplier = 0.195
+ac_offset = 32768
+dc_uV_multiplier = -19.23
+dc_offset = 512
+num_channels = 32
 
-suffix = '0'; # Change to match file names' suffix
-# Change this to the directory of your data. In this example, data's in the same directory as this data loading Python script
-data_directory = os.path.dirname(os.path.realpath(__file__))
-start_t = 1.0 # when to start plotting data (seconds)
-dur = 1.0 # duration of data to plot
-
-#%% Metadata
-
+# Load acqusition session data
 dt = {'names': ('time', 'acq_clk_hz', 'block_read_sz', 'block_write_sz'),
       'formats': ('datetime64[us]', 'u4', 'u4', 'u4')}
-meta = np.genfromtxt(os.path.join(data_directory, f'start-time_{suffix}.csv'), delimiter=',', dtype=dt)
+meta = np.genfromtxt(f'c:/Users/cshor/Downloads/data/rhs2116-data/start-time_{suffix}.csv', delimiter=',', dtype=dt, skip_header=1)
 print(f"Recording was started at {meta['time']} GMT")
 
-#%% RHS2116 ephys data
+# Load RHS2116 clock data
+time = np.fromfile(f'c:/Users/cshor/Downloads/data/rhs2116-data/rhs2116-clock_{suffix}.raw', dtype=np.uint64) / meta['acq_clk_hz']
 
-start_t = 1.0 # when to start plotting data (seconds)
-dur = 1.0 # duration of data to plot
+# Load RHS2116 ac data
+ac = np.reshape(np.fromfile(f'c:/Users/cshor/Downloads/data/rhs2116-data/rhs2116-ac_{suffix}.raw', dtype=np.uint16), (-1, num_channels))
 
-rhs2116 = {}
-rhs2116['time'] = np.fromfile(os.path.join(data_directory, f'rhs2116-clock_{suffix}.raw'), dtype=np.uint64) / meta['acq_clk_hz']
-b = np.bitwise_and(rhs2116['time'] >= start_t, rhs2116['time'] < start_t + dur)
-time = rhs2116['time'][b]
+# Truncate data based on start_t & dur
+b = np.bitwise_and(time >= start_t, time < start_t + dur)
+ac_truncated = ac[b,:].astype(np.double)
+time_truncated = time[b]
 
-# AC data
-rhs2116['ac'] = np.reshape(np.fromfile(os.path.join(data_directory, f'rhs2116-ac_{suffix}.raw'), dtype=np.uint16), (-1, 32))
-rhs2116_ac = rhs2116['ac'][b, :].astype(np.double)
-bit_depth = 16
-scalar = 0.195
-offset = (2 ** (bit_depth - 1)) * scalar
-rhs2116_ac_converted = rhs2116_ac * scalar - offset + np.arange(rhs2116_ac.shape[1])[None, :] * offset / 4
+# Scale truncated RHS2116 ac data
+ac_truncated_scaled = (ac_truncated - ac_offset) * ac_uV_multiplier
 
-# DC data
-rhs2116['dc'] = np.reshape(np.fromfile(os.path.join(data_directory, f'rhs2116-dc_{suffix}.raw'), dtype=np.uint16), (-1, 32))
-rhs2116_dc = rhs2116['dc'][b, :].astype(np.double)
-bit_depth = 10
-scalar = -19.23
-offset = (2 ** (bit_depth - 1)) * -scalar
-rhs2116_dc_converted = rhs2116_dc * scalar - offset + np.arange(rhs2116_dc.shape[1])[None, :] * offset / 4
-
-def setup_subplot(ax):
-      ax.set_yticks([])
-      ax.set_ylabel("channel")
-      ax.set_xlabel("time (s)")
-
-fig, (ax_ac, ax_dc) = plt.subplots(nrows=1, ncols=2, figsize=(10,8))
-ax_ac.plot(time, rhs2116_ac_converted, 'k', linewidth=0.25)
-ax_ac.set_title('RHS2116 AC Data')
-setup_subplot(ax_ac)
-ax_dc.plot(time, rhs2116_dc_converted, 'k', linewidth=0.25)
-ax_dc.set_title('RHS2116 DC Data')
-setup_subplot(ax_dc)
-fig.suptitle('RHS2116 Data')
+# Plot RHS2116 ac data
+fig = plt.figure()
+fig.suptitle("RHS2116 AC Data")
+plt.plot(time_truncated, ac_truncated_scaled[:,0:plot_num_channels])
+plt.xlabel("Time (seconds)")
+plt.ylabel("Voltage (µV)")
 plt.tight_layout()
+
+# Load RHS2116 dc data
+dc = np.reshape(np.fromfile(f'c:/Users/cshor/Downloads/data/rhs2116-data/rhs2116-dc_{suffix}.raw', dtype=np.uint16), (-1, num_channels))
+
+# Truncate RHS2116 dc data
+dc_truncated = dc[b,:].astype(np.double)
+
+# Scale truncated RHD2164 dc data
+dc_truncated_scaled = (dc_truncated - dc_offset) * dc_uV_multiplier
+
+# Plot RHS2116 dc data
+fig = plt.figure()
+fig.suptitle("RHS2116 DC Data")
+plt.plot(time_truncated, dc_truncated_scaled[:,0:plot_num_channels])
+plt.xlabel("Time (seconds)")
+plt.ylabel("Voltage (µV)")
+plt.tight_layout()
+
 plt.show()
